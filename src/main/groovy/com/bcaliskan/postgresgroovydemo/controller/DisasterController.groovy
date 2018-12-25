a62075c64fcf989172b166a185b8fd9e06b742f8
= <bilalccaliskan@gmail.com>
2018-12-26 01:07:50 +0300

    controllers implemented

--- src/main/groovy/com/bcaliskan/postgresgroovydemo/controller/DisasterController.groovy
+++ src/main/groovy/com/bcaliskan/postgresgroovydemo/controller/DisasterController.groovy
@@ -1,4 +1,66 @@
 package com.bcaliskan.postgresgroovydemo.controller
 
+import com.bcaliskan.postgresgroovydemo.model.Response
+import com.bcaliskan.postgresgroovydemo.persistence.entity.Disaster
+import com.bcaliskan.postgresgroovydemo.persistence.service.DisasterService
+import groovy.json.JsonOutput
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping('disaster-controller')
 class DisasterController {
+
+    @Autowired
+    DisasterService disasterService
+
+
+    @GetMapping('')
+    List findAll() {
+        return disasterService.findAll()
+    }
+
+    @GetMapping('{id}')
+    Disaster findById(@PathVariable long id) {
+        return disasterService.findByIdOrReturnNull(id)
+    }
+
+    @PostMapping('create-disaster')
+    String create(@RequestBody Disaster disaster) {
+        return JsonOutput.toJson(new Response("createDisaster", disasterService.save(disaster)))
+    }
+
+    @PutMapping('update-disaster/{id}')
+    String update(@RequestBody Disaster disaster) {
+        return JsonOutput.toJson(new Response("updateDisaster", disasterService.update(disaster)))
+    }
+
+    @DeleteMapping('delete-disaster/{id}')
+    String deleteById(@PathVariable long id) {
+        return JsonOutput.toJson(new Response("deleteDisaster", disasterService.deleteById(id)))
+    }
+
+    @PostMapping('assign-hero/{id}/hero/{heroId}')
+    String assignHero(@PathVariable long id, @PathVariable long heroId) {
+        return JsonOutput.toJson(new Response("assignHero", disasterService.assignHero(id, heroId)))
+    }
+
+    @DeleteMapping('remove-hero/{id}/hero/{heroId}')
+    String removeHero(@PathVariable long id, @PathVariable long heroId) {
+        return JsonOutput.toJson(new Response("removeHero", disasterService.removeHero(id, heroId)))
+    }
+
+    @PostMapping('resolve-disaster/{id}')
+    String resolveDisaster(@PathVariable long id) {
+        return JsonOutput.toJson(new Response("resolveDisaster", disasterService.resolve(id)))
+    }
+
 }
--- src/main/groovy/com/bcaliskan/postgresgroovydemo/controller/HeroController.groovy
+++ src/main/groovy/com/bcaliskan/postgresgroovydemo/controller/HeroController.groovy
@@ -1,4 +1,51 @@
 package com.bcaliskan.postgresgroovydemo.controller
 
+import com.bcaliskan.postgresgroovydemo.model.Response
+import com.bcaliskan.postgresgroovydemo.persistence.entity.Hero
+import com.bcaliskan.postgresgroovydemo.persistence.service.HeroService
+import groovy.json.JsonOutput
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("hero-controller")
 class HeroController {
+
+    @Autowired
+    HeroService heroService
+
+
+    @GetMapping('')
+    List findAll() {
+        return heroService.findAll()
+    }
+
+    @GetMapping('{id}')
+    Hero findOne(@PathVariable long id) {
+        return heroService.findByIdOrReturnNull(id)
+    }
+
+    @PostMapping('')
+    String create(@RequestBody Hero hero) {
+        return JsonOutput.toJson(new Response("createHero", heroService.save(hero)))
+    }
+
+    @PutMapping('')
+    String update(@RequestBody Hero hero) {
+        return JsonOutput.toJson(new Response("updateHero", heroService.update(hero)))
+    }
+
+    @DeleteMapping('{id}')
+    String deleteById(@PathVariable long id) {
+        return JsonOutput.toJson(new Response("deleteHero", heroService.deleteById(id)))
+    }
+
 }
